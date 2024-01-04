a4d52156e8918cc2880b326650ee4b392cb62605
MarieNRICH <pofatu@gmail.com>
2024-01-04 15:54:46 +0100

    auth en place

--- app/Models/Skill.php
+++ app/Models/Skill.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Skill extends Model
 {
     use HasFactory;
+
+    // nom au pluriel car un skill peut regper pls users
+    // cardinalité 1,n
+
+    public function heroe()
+    {
+        return $this->hasMany(Heroe::class);
+    }
 }
